9265f2c19b4b71b9dd3e74ded0582e0693fbcb16
alonavns <alona.vns@outlook.com>
2018-07-08 14:39:17 +0300

    clean up orders

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,12 +1,8 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import PropTypes from 'prop-types'
-import { compose } from 'recompose'
 
 import { Lendroid } from '../_lendroid-js'
-
-import { connectContract } from '../../redux/modules'
-import { promisify } from '../../utilities'
 
 import TableGroup from '../TableGroup/TableGroup'
 import ListGroup from '../ListGroup/ListGroup'
@@ -38,98 +34,11 @@ class Orders extends Component {
     this.apiPost = this.apiPost.bind(this)
   }
 
-  componentDidMount() {
-    const { address, network } = this.props
-
-    this.getContracs(address, network)
-  }
-
   componentWillReceiveProps(newProps) {
     const { address, network } = this.props
 
     if (newProps.address !== address || newProps.network !== network) {
       this.state.LendroidJS.reset({ address: newProps.address, network: newProps.network }, () => this.forceUpdate())
-      this.getContracs(newProps.address, newProps.network)
-    }
-  }
-
-  getContracs(address, network) {
-    if (network) {
-      const {
-        contractETHBlance,
-        contractFetchWeth,
-        contractFetchDai,
-        contractFetchLst,
-        contractFetchLoanOfferRegistry,
-        contractFetchLoanRegistry,
-        contractFetchLoan,
-        contractFetchWranglerLoanRegistry,
-        tokenBalanceWeth,
-        tokenBalanceDai,
-        tokenBalanceLst,
-        tokenAllowanceWeth,
-        tokenAllowanceDai,
-        tokenAllowanceLst,
-      } = this.props
-      const { web3 } = window
-
-      promisify(contractETHBlance, { web3, address })
-        .then(res => { /*console.log(res)*/ })
-        .catch(e => { console.log(e) })
-
-      promisify(contractFetchWeth, { web3, network })
-        .then(res => {
-          promisify(tokenBalanceWeth, { web3, contractInstance: res, address })
-            .then(res => { /*console.log(res)*/ })
-            .catch(e => { console.log(e) })
-
-          promisify(tokenAllowanceWeth, { web3, contractInstance: res, address })
-            .then(res => { /*console.log(res)*/ })
-            .catch(e => { console.log(e) })
-        })
-        .catch(e => { console.log(e) })
-
-      promisify(contractFetchDai, { web3, network })
-        .then(res => {
-          promisify(tokenBalanceDai, { web3, contractInstance: res, address })
-            .then(res => { /*console.log(res)*/ })
-            .catch(e => { console.log(e) })
-
-          promisify(tokenAllowanceDai, { web3, contractInstance: res, address })
-            .then(res => { /*console.log(res)*/ })
-            .catch(e => { console.log(e) })
-        })
-        .catch(e => { console.log(e) })
-
-      promisify(contractFetchLst, { web3, network })
-        .then(res => {
-          promisify(tokenBalanceLst, { web3, contractInstance: res, address })
-            .then(res => { /*console.log(res)*/ })
-            .catch(e => { console.log(e) })
-
-          promisify(tokenAllowanceLst, { web3, contractInstance: res, address })
-            .then(res => { /*console.log(res)*/ })
-            .catch(e => { console.log(e) })
-        })
-        .catch(e => { console.log(e) })
-
-      promisify(contractFetchLoanOfferRegistry, { web3, network })
-        .then(res => { /*console.log(res)*/ })
-        .catch(e => { console.log(e) })
-
-      promisify(contractFetchLoanRegistry, { web3, network })
-        .then(res => {
-          /*console.log(res)*/
-
-          promisify(contractFetchLoan, { web3, network })
-            .then(res => { /*console.log(res)*/ })
-            .catch(e => { console.log(e) })
-        })
-        .catch(e => { console.log(e) })
-
-      promisify(contractFetchWranglerLoanRegistry, { web3, network })
-        .then(res => { /*console.log(res)*/ })
-        .catch(e => { console.log(e) })
     }
   }
 
@@ -221,6 +130,4 @@ Orders.contextTypes = {
   web3: PropTypes.object
 }
 
-export default compose(
-  connectContract(),
-)(Orders)
+export default Orders
