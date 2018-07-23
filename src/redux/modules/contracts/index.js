cd12a9746ee376b59c3acc485cab551574772f98
alonavns <alona.vns@outlook.com>
2018-07-23 13:24:53 +0300

    remove redux

--- src/redux/modules/contracts/index.js
+++
@@ -1,4 +0,0 @@
-export { default as contracts } from './reducer'
-export { connectContract } from './connect'
-export { contractActionCreators } from './actions'
-export { default as contractSaga } from './saga'
