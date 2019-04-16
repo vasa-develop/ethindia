33caea20bafa864c2342dfb83049051d2c1c2d87
alonavns <alona.vns@outlook.com>
2019-04-16 17:36:34 +0300

    contracts update

--- src/components/Orders/Contracts.js
+++ src/components/Orders/Contracts.js
@@ -4,16 +4,18 @@ export const CONTRACT_ADDRESSES = {
   DAI: {
     1: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
     42: '0xC4375B7De8af5a38a93548eb8453a498222C4fF2',
-    def: ABIs.DAIABI
+    def: ABIs.ERC20ABI
+    // def: ABIs.DAIABI
   },
   DGX: {
     1: '0x4f3AfEC4E5a3F2A6a1A411DEF7D7dFe50eE057bF',
     42: '0x7d6D31326b12B6CBd7f054231D47CbcD16082b71',
-    def: ABIs.DGXABI
+    def: ABIs.ERC20ABI
+    // def: ABIs.DGXABI
   },
   USDC: {
     1: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-    42: '0x818AE72AF93bBF36f605c61b21Ab5b44fc22827d',
+    42: '0x507aac698a8f2dcd2a6f48090972cdcd5342e5f9',
     def: ABIs.ERC20ABI
   }
 }
